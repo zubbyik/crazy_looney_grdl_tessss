a70fca6cd53a5f7674583fe19075a460f11bb3e5
Malachy Egbuna <zubbyik@gmail.com>
2021-08-06 20:43:06 +0100

    some more changes to sourcefile

--- src/test/java/learn/tut/WebTest.java
+++ src/test/java/learn/tut/WebTest.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
+
 public class WebTest {
 
     @Test
@@ -14,6 +16,7 @@ public class WebTest {
             Browser browser = playwright.firefox().launch();
             Page page = browser.newPage();
             page.navigate(url);
+            page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("src/test/resources/playwright_capture.png")).setFullPage(true));
             System.out.println(page.url());
             System.out.println(page.title());
         }
