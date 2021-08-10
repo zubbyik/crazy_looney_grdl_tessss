6b047f4a45bf8be473bbf2f99599b496cc58b220
Malachy Egbuna <zubbyik@gmail.com>
2021-08-10 01:00:56 +0100

    src/test/java

--- src/test/java/learn/tut/WebTest.java
+++ src/test/java/learn/tut/WebTest.java
@@ -12,6 +12,7 @@ public class WebTest {
     @Test
     public void checkWeb(){
         try(Playwright playwright = Playwright.create()){
+            String url = null;
             Browser browser = playwright.firefox().launch();
             Page page = browser.newPage();
             page.navigate(url);
