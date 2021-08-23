de0dc8447706771fbd759a3285cae6beaf4de0f7
Malachy Egbuna <zubbyik@gmail.com>
2021-08-23 22:04:06 +0100

    modified build.gradle

---
+++ src/test/java/MyStepdefs.java
@@ -0,0 +1,13 @@
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+    @Then("the Maker waits for a Breaker to join")
+    public void theMakerWaitsForABreakerToJoin() {
+        
+    }
+
+    @When("the Maker starts a game")
+    public void theMakerStartsAGame() {
+    }
+}
--- src/test/java/StepDefinition/Main.java
+++ src/test/java/StepDefinition/Main.java
@@ -1,4 +1,4 @@
-package learn.cucks;
+package StepDefinition;
 
 public class Main {
     public static void main(String[] args) {
