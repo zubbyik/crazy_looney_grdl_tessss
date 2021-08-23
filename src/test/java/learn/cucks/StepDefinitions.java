58cca96dd762b5fefb1159b55dbd948c0aa6ba25
Malachy Egbuna <zubbyik@gmail.com>
2021-08-23 10:48:29 +0100

    modified gradle.build, source-files

--- src/test/java/learn/cucks/StepDefinitions.java
+++ src/test/java/learn/cucks/StepDefinitions.java
@@ -12,19 +12,19 @@ public class StepDefinitions {
     @Given("Today is Sunday")
     public void today_is_sunday() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("completed");
     }
 
     @When("I ask whether it's Friday Yet?")
     public void i_ask_whether_it_s_friday_yet() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("completed");
     }
 
     @Then("I should be told {string}")
     public void i_should_be_told(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("completed");
     }
 
 
